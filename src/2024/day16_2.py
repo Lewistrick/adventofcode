3de9bf3f96afc6cdd1ccea025f23508a005d3025
Erick Wilts <erick.wilts@devops.ncia.nato.int>
2024-12-16 14:18:34 +0100

    Small improvements

--- src/2024/day16_2.py
+++ src/2024/day16_2.py
@@ -1,12 +1,15 @@
+import random
 import time
+from functools import cache
 from heapq import heappop, heappush
 from typing import Generator
 
 from aoc_tools import NESW, read_data
+from loguru import logger
 
 Pos = tuple[int, int]
 
-data = read_data(suffix="in").split("\n")
+data = read_data(day=16, suffix="in").split("\n")
 
 tiles = set()
 for y, row in enumerate(data):
@@ -23,42 +26,68 @@ for y, row in enumerate(data):
 
 part1 = 0
 
+@cache
+def getprio(cx: int, cy: int, ex: int, ey: int) -> int:
+    """Heuristic function accounting for turn and straight movement costs."""
+    return abs(cx - ex) + abs(cy - ey)
+
 
 def get_solutions(
     sx: int, sy: int, ex: int, ey: int, tiles: set[Pos]
 ) -> Generator[tuple[set[Pos], int], None, None]:
-    queue = [(0, sx, sy, 0, 0, {(sx, sy)})]  # (cx, cy, score, dx, dy, seen)
-    shortest_score = float("inf")
+    queue = [(0, 0, sx, sy, 0, 0, {(sx, sy)})]  # (prio, score, cx, cy, dx, dy, seen)
+    shortest_score = 220_000
     shortest_to = {}  # {(cx, cy, dx, dy): dist}
 
-    next_print = time.perf_counter()
+    t0 = time.perf_counter()
+    stepx = 0
+    next_print = 1
     while queue:
-        score, cx, cy, dx, dy, seen = heappop(queue)
+        stepx += 1
+        prio, score, cx, cy, dx, dy, seen = heappop(queue)
 
-        if time.perf_counter() > next_print:
-            next_print += 5
-            print(f"{next_print:.0f} {len(queue)=} {score=}")
+        if stepx >= next_print:
+            next_print *= random.random() / 2 + 1
+            tx = time.perf_counter()
+            logger.debug(f"{tx-t0:.1f} {stepx=} {len(queue)=} {score=}")
 
         if score > shortest_score:
             continue
 
-        if (olddist := shortest_to.get((cx, cy, dx, dy))) and olddist < score:
+        if (
+            olddist := shortest_to.get((cx, cy, dx, dy), shortest_score)
+        ) and olddist < score:
             continue
         shortest_to[(cx, cy, dx, dy)] = olddist
 
         # Reached the end position
         if (cx, cy) == (ex, ey):
             shortest_score = score
-            print(f"Found new solution with score {score}")
+            tx = time.perf_counter()
+            logger.debug(f"{tx-t0:.1f} {stepx=} {len(queue)=} {score=}")
+            logger.success(f"Found new solution with score {score}")
             yield seen, score
             continue
 
         # Go straight
         newpos = (cx + dx, cy + dy)
         if newpos in tiles and newpos not in seen:
-            if (olddist := shortest_to.get((*newpos, dx, dy))) and olddist < score + 1:
+            if (
+                olddist := shortest_to.get((*newpos, dx, dy), shortest_score)
+            ) and olddist < score + 1:
                 continue
-            heappush(queue, (score + 1, newpos[0], newpos[1], dx, dy, seen | {newpos}))
+            heappush(
+                queue,
+                (
+                    score + getprio(*newpos, ex, ey),
+                    score + 1,
+                    newpos[0],
+                    newpos[1],
+                    dx,
+                    dy,
+                    seen | {newpos},
+                ),
+            )
 
         # Explore all valid moves
         for ndx, ndy in NESW:
@@ -76,6 +105,7 @@ def get_solutions(
                 heappush(
                     queue,
                     (
+                        score + getprio(*newpos, ex, ey),
                         score + 1001,
                         *newpos,
                         ndx,
@@ -83,6 +113,8 @@ def get_solutions(
                         seen | {newpos},
                     ),
                 )
+
+    logger.info(f"Took {time.perf_counter() - t0} seconds")
 
 
 sols = []
@@ -92,6 +124,7 @@ for sol, score in get_solutions(sx, sy, ex, ey, tiles):
     shortest = min(score, shortest)
 
 part1 = shortest
+logger.info(f"Part 1: {part1}")
 
 sols2 = [sol for sol, score in sols if score == shortest]
 
@@ -101,5 +134,4 @@ for sol, score in sols:
         allseen |= sol
 
 part2 = len(allseen)
-print()
-print(part1, part2)
+logger.info(f"Part 2: {part2}")
