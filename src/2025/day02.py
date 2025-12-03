7fed326c0dcc7da9709e920521f04307a4b6d278
Erick Wilts <erick.wilts@devops.ncia.nato.int>
2025-12-03 10:17:43 +0100

    Solutions and example data for day 1-3 of 2025

---
+++ src/2025/day02.py
@@ -0,0 +1,47 @@
+from functools import cache
+from math import log10
+
+from aoc_tools import read_data
+from loguru import logger
+
+data = read_data(suffix="in").split(",")
+
+
+@cache
+def get_divisors(n):
+    divisors = []
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            divisors.append(i)
+
+    return divisors
+
+
+part1 = 0
+part2 = 0
+for rng in data:
+    # logger.debug(f"{rng=}")
+    low, high = rng.split("-")
+
+    for n in range(int(low), int(high) + 1):
+        ndigits = int(log10(n)) + 1
+        if ndigits == 1:
+            continue
+
+        added = False
+        for sublen in get_divisors(ndigits):
+            s = str(n)
+            parts = {s[i * sublen : i * sublen + sublen] for i in range(ndigits//sublen)}
+            if len(parts) == 1:
+                if not added:
+                    part2 += n
+                    # logger.success(f"Invalid (part 2): {n} {parts=}")
+
+                added = True
+                if sublen == ndigits / 2:
+                    # logger.info(f"Invalid (part 1): {n} {parts=}")
+                    part1 += n
+
+
+logger.success(part1)
+logger.success(part2)
